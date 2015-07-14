814d6b0c61769d936a69a87a660361a72c684b94
ranjeet <amitlranjeet_5619@yahoo.co.in>
2015-07-14 00:16:28 -0800

    test1
    test

---
+++ Example1/movies.js
@@ -0,0 +1,10 @@
+function printAvatar(){
+    console.log("Avatar: Avatar 2D, 3D");
+}
+
+function printChappie(){
+    console.log("Matrix: Reloaded");
+}
+
+//What gets exported is determined by 'module.exports' variable
+module.exports.avatar = printAvatar;
